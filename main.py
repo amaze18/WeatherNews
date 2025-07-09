86b47406a5828a3299f91df31e9525150ce8297e
Likhith623 <likhithchowdary_vasireddy@srmap.edu.in>
2025-07-10 00:17:12 +0530

    g

--- main.py
+++ main.py
@@ -197,6 +197,3 @@ def insert_bot_message(email, bot_id, message):
     }).execute()
     
     
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
